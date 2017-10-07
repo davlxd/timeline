71171c1259a8396328a5e9bd62302f675230db75
lxd <i@lxd.me>
2017-10-08 10:18:26 +1300

    Tune EditPanel a little bit

--- src/containers/EditPanel/index.js
+++ src/containers/EditPanel/index.js
@@ -2,20 +2,22 @@ import React from 'react'
 import SlidingPane from 'react-sliding-pane'
 import 'react-sliding-pane/dist/react-sliding-pane.css'
 
+import './style.css'
+
 const EditPanel = ({ isPaneOpen, onRequestClose }) => (
   <div >
     <SlidingPane
-      className='some-custom-class'
-      overlayClassName='some-custom-overlay-class'
+      className="some-custom-class"
+      overlayClassName='SlidePaneOverlay'
       isOpen={ isPaneOpen }
       title='Hey, it is optional pane title.  I can be React component too.'
       subtitle='Optional subtitle.'
-      width='50%'
+      width='38.2%'
       onRequestClose={ onRequestClose }
       >
         <div>And I am pane content. BTW, what rocks?</div>
         <br />
-      </SlidingPane>
+    </SlidingPane>
   </div>
 )
 
