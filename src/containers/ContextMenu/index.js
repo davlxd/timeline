d919c81985892575f0ab3fcb2f27d19c6478b67c
lxd <i@lxd.me>
2017-11-07 22:31:22 +1300

    Initial version of ContextMenu

--- src/containers/ContextMenu/index.js
+++ src/containers/ContextMenu/index.js
@@ -6,7 +6,9 @@ import { grey800 } from 'material-ui/styles/colors'
 
 let ContextMenu = ({ open, mouseX, mouseY }) => {
   const x = mouseX
-  const y = mouseY
+  const y = mouseY //TODO
+  const width = 200, height = 140, menuEntryTexHeight = 28, menuEntryTextFontFamily = 'Roboto, sans-serif'
+  const menuEntryTextSize = 15, menuEntryPadding = 20
   return (
     <Group
       visible={open}
@@ -15,14 +17,62 @@ let ContextMenu = ({ open, mouseX, mouseY }) => {
         x={x}
         y={y}
         fill='#ffffff'
-        width={200}
-        height={250}
+        width={width}
+        height={height}
         shadowColor={grey800}
-        shadowBlur={10}
+        shadowBlur={2}
         shadowOffset={[10, 10]}
         shadowOpacity={0.2}
         cornerRadius={2}
       />
+      <Text
+          x={x}
+          y={y}
+          text={'\u2328 TextBox'}
+          fontSize={menuEntryTextSize}
+          fontFamily={menuEntryTextFontFamily}
+          fill='#555'
+          width={width}
+          padding={menuEntryPadding}
+          align='left'
+          ref={(text) => {this.canvasText = text}}
+      />
+      <Text
+          x={x}
+          y={y + menuEntryTexHeight * 1}
+          text={'\u2328 Detached TextBox'}
+          fontSize={menuEntryTextSize}
+          fontFamily={menuEntryTextFontFamily}
+          fill='#555'
+          width={width}
+          padding={menuEntryPadding}
+          align='left'
+          ref={(text) => {this.canvasText = text}}
+      />
+      <Text
+          x={x}
+          y={y + menuEntryTexHeight * 2}
+          text={'\u27F7 Range'}
+          fontSize={menuEntryTextSize}
+          fontFamily={menuEntryTextFontFamily}
+          fill='#555'
+          width={width}
+          padding={menuEntryPadding}
+          align='left'
+          ref={(text) => {this.canvasText = text}}
+      />
+      <Text
+          x={x}
+          y={y + menuEntryTexHeight * 3}
+          text={'\u26CA Milestone'}
+          fontSize={menuEntryTextSize}
+          fontFamily={menuEntryTextFontFamily}
+          fill='#555'
+          width={width}
+          padding={menuEntryPadding}
+          align='left'
+          ref={(text) => {this.canvasText = text}}
+        />
     </Group>
   )
 }
