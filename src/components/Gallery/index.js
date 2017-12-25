4a76c8b53ac50e4dcee4df37424ae95d21dd2468
lxd <i@lxd.me>
2017-12-25 19:52:52 +1300

    Gallery first step

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import Paper from 'material-ui/Paper'
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+import './style.css'
+
+const style = {
+  height: 300,
+  width: 533,
+  margin: 20,
+  textAlign: 'center',
+  display: 'inline-block',
+};
 
 const Gallery = () => (
    <div>
-     Gallery
+     <MuiThemeProvider>
+       <div>
+         <Paper className="Card" style={style}>
+           <span className="CardTitle"> Text </span>
+         </Paper>
+         <Paper className="Card" style={style}>
+           <span className="CardTitle"> Text </span>
+         </Paper>
+         <Paper className="Card" style={style}>
+           <span className="CardTitle"> Text </span>
+         </Paper>
+         <Paper className="Card" style={style}>
+           <span className="CardTitle"> Text </span>
+         </Paper>
+         <Paper className="Card" style={style}>
+           <span className="CardTitle"> Text </span>
+         </Paper>
+         <Paper className="Card" style={style}>
+           <span className="CardTitle"> Text </span>
+         </Paper>
+       </div>
+     </MuiThemeProvider>
+
    </div>
 )
 
