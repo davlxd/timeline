43b04b7c3bb197b7a0cbd3b2d213175c664a42b7
lxd <i@lxd.me>
2017-12-26 21:32:47 +1300

    Make logo smaller

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,29 +1,21 @@
 import React from 'react'
 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import RaisedButton from 'material-ui/RaisedButton'
-
 import './style.css'
 
-const buttonStyle = {
-  zIndex: 1
-}
-
 const Home = () => (
-   <div className="bg">
-     <video className="bgv" loop autoPlay>
-                <source src={process.env.PUBLIC_URL + '/one-shot-fast.mp4'} type="video/mp4" />
-                {/* <source src={this.state.videoURL} type="video/ogg" /> */}
-                Your browser does not support the video tag.
-      </video>
-      <div className="fg"></div>
-      <span className="logo">Timeline.ink</span>
-        <div className="buttons">
-          <span className="button try"><a className="button-link" href="/line/ca8455706902446396d381363bd869a7">Try it out!</a></span>
-          <span className="button gallery"><a className="button-link" href="/gallery">Browse Gallery</a></span>
-        </div>
-   </div>
-
+  <div className="bg">
+    <video className="bgv" loop autoPlay>
+      <source src={process.env.PUBLIC_URL + '/one-shot-fast.mp4'} type="video/mp4" />
+      {/* <source src={this.state.videoURL} type="video/ogg" /> */}
+      Your browser does not support the video tag.
+    </video>
+    <div className="fg"></div>
+    <span className="logo">Timeline.ink</span>
+    <div className="buttons">
+      <span className="button try"><a className="button-link" href="/line/ca8455706902446396d381363bd869a7">Try it out!</a></span>
+      <span className="button gallery"><a className="button-link" href="/gallery">Browse Gallery</a></span>
+    </div>
+  </div>
 )
 
 export default Home
