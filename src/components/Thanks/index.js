b6c60b579287a3ac9f252f46b6d12477b2c9210b
lxd <i@lxd.me>
2017-12-29 10:33:40 +1300

    Add Material-UI to thanks

--- src/components/Thanks/index.js
+++ src/components/Thanks/index.js
@@ -7,11 +7,12 @@ const Thanks = () => (
      <h3>Thanks</h3>
      <p>Timeline.ink is made possible by many wonderful open source projects:</p>
      <ul>
-       <li><a href="https://github.com/lavrton/react-konva">React Konva</a>, license under <a href="https://github.com/lavrton/react-konva/blob/master/LICENSE">MIT</a>.</li>
-       <li><a href="https://konvajs.github.io/">Konvajs</a>, license under <a href="https://github.com/konvajs/konva/wiki/License">MIT</a>.</li>
-       <li><a href="https://reactjs.org/">React</a>, license under <a href="https://github.com/facebook/react#license">MIT</a>.</li>
-       <li><a href="https://redux.js.org/">Redux</a>, license under <a href="https://redux.js.org/#license">MIT</a>.</li>
+       <li><a href="https://github.com/lavrton/react-konva">React Konva</a>, licensed under <a href="https://github.com/lavrton/react-konva/blob/master/LICENSE">MIT</a>.</li>
+       <li><a href="https://konvajs.github.io/">Konvajs</a>, licensed under <a href="https://github.com/konvajs/konva/wiki/License">MIT</a>.</li>
+       <li><a href="https://reactjs.org/">React</a>, licensed under <a href="https://github.com/facebook/react#license">MIT</a>.</li>
+       <li><a href="https://redux.js.org/">Redux</a>, licensed under <a href="https://redux.js.org/#license">MIT</a>.</li>
        <li><a href="https://wordpress.com/">WordPress</a>, from <a href="https://automattic.com/">Automattic</a>.</li>
+       <li><a href="http://www.material-ui.com">Material-UI</a>, licensed under <a href="https://github.com/mui-org/material-ui#license">MIT</a>.</li>
      </ul>
    </div>
 )
